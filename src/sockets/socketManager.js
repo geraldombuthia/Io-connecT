fcb5a9af0bcd7ba5186842eaa2298d77569cae4d
geraldombuthia <geraldombuthia@gmail.com>
2024-10-16 09:58:18 +0300

    f/add-socket-implementation: Implemented socket data reception from client. Time received as seconds but new Date() takes in milliseconds. Will fully implement socket data storage, command transfer and diversity control (Requests and responses can be sent out based on user/device e.t.c

--- src/sockets/socketManager.js
+++ src/sockets/socketManager.js
@@ -1,11 +1,11 @@
 //@TODO Convert to a class to allow variable functionality
 
 let AlertSeverity = Object.freeze({
-  critical: 'critical',
-  error: 'error',
-  warning: 'warning',
-  info: 'info'
-})
+  critical: "critical",
+  error: "error",
+  warning: "warning",
+  info: "info",
+});
 
 let AlertType = Object.freeze({
   notif: "notification",
@@ -16,47 +16,53 @@ let AlertType = Object.freeze({
   sms: "sms",
   email: "email",
   shutdown: "shutdown",
-  restart: "restart"
-})
+  restart: "restart",
+});
 
 function socket() {
+  console.log("Attempting to connect to client");
   const WebSocket = require("ws");
 
   const wss = new WebSocket.Server({ port: 8081 });
+  try {
+    wss.on("connection", (ws) => {
+      console.log("New client connected");
 
-  wss.on("connection", (ws) => {
-    console.log("New client connected");
+      let welcomeText = {
+        message: "welcome to the IoConnect server",
+        timestamp: new Date(),
+        timeformat: "locale_string",
+        senderId: "server",
+      };
 
-    let welcomeText = {
-      message: "welcome to the IoConnect server",
-      timestamp: new Date().toLocaleString(),
-      timeformat: "locale_string",
-      senderId: "server",
-    };
+      // Send a welcome message to the connected client
+      ws.send(JSON.stringify(welcomeText));
 
-    // Send a welcome message to the connected client
-    ws.send(JSON.stringify(welcomeText));
+      // Listen for messages from the client
+      ws.on("message", (message) => {
+        console.log(message);
+        let payload = JSON.parse(message);
 
-    // Listen for messages from the client
-    ws.on("message", (message) => {
-      let payload = JSON.parse(message);
-      console.log(
-        `Received msg: ${payload.message} sent at ${payload.timestamp} by ${payload.senderId}`
-      );
+        console.log(
+          `Received msg: ${payload.message} sent at ${new Date(payload.timestamp * 1000).toLocaleString()} by ${payload.senderId}\n`
+        );
 
-      // Send the received message back to all connected clients (broadcast)
-      wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(`${JSON.stringify(payload)}`);
-        }
+        // Send the received message back to all connected clients (broadcast)
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(`${JSON.stringify(payload)}`);
+          }
+        });
+      });
+
+      // Handle the connection close event
+      ws.on("close", () => {
+        console.log("Client disconnected");
       });
     });
-
-    // Handle the connection close event
-    ws.on("close", () => {
-      console.log("Client disconnected");
-    });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = socket;
